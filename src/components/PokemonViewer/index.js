38768cc3dcb1d09e379ddbcc7ae3a44093e25ce2
James Attrill <attrill20@gmail.com>
2023-04-17 20:08:01 +0100

    task 1 done

--- src/components/PokemonViewer/index.js
+++ src/components/PokemonViewer/index.js
@@ -1,8 +1,38 @@
+import React, {useEffect, useState} from "react";
+
 function PokemonViewer({ id }) {
-  //TODO: Task 1 - send http request to `https://pokeapi.co/api/v2/pokemon/1' and display the data
-  // TODO: Task 2 - send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
-  // HINT: you will need useState and useEffect!
-  return <div className="pokemon-viewer"></div>;
+
+  const[pokemon, setPokemon] = useState()
+
+  useEffect(()=>{
+    async function pokemonFetch(){
+        const res = await fetch ("https://pokeapi.co/api/v2/pokemon/1");
+        const data =await res.json();
+        console.log(data)
+        setPokemon(data)
+      }
+      pokemonFetch()
+  }
+   ,[id]);
+
+  return (
+  <div className="pokemon-viewer">  
+  {pokemon && (
+        <>
+          <h2> Name: {pokemon.name} </h2>
+          <p> ID: {pokemon.id} </p>
+          <p> Weight: {pokemon.weight} </p>
+          </>
+      )}
+    </div>
+  );
 }
 
 export default PokemonViewer;
+
+
+
+  //TODO: Task 1 - send http request to `https://pokeapi.co/api/v2/pokemon/1' and display the data
+
+  // TODO: Task 2 - send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
+  // HINT: you will need useState and useEffect!
